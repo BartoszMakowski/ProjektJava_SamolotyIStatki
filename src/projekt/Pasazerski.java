5946eb61185094e27b1c18ad7c0c5fece208a428
Bartosz <baboshpl@gmail.com>
2016-01-06 14:29:44 +0100

    utworzono interfejs "Pasazerski"

---
+++ src/projekt/Pasazerski.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt;
+
+/**
+ *
+ * @author bartosz
+ */
+public interface Pasazerski {
+    
+    public void przesiadkaPasazera(Podrozny pasazer, Pasazerski dokad);    
+    public void dodajPasazera(Podrozny pasazer);
+    public void usunPasazera(Podrozny pasazer);
+}
