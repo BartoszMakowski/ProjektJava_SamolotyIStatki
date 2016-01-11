f0c0b1533e61f9b674d97300b991885f1130b61a
Bartosz <baboshpl@gmail.com>
2016-01-11 02:05:07 +0100

    zaimplementowano konstruktor, dodano metody interfejsu "Pasażerski"

--- src/projekt/Wycieczkowiec.java
+++ src/projekt/Wycieczkowiec.java
@@ -1,5 +1,6 @@
 package projekt;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -13,6 +14,22 @@ public class Wycieczkowiec extends Statek{
 
     public Wycieczkowiec(Polozenie polozenie, int predkosc, PortMorski najblizszyCel, List<Lokalizacja> trasa) {
         super(polozenie, predkosc, najblizszyCel, trasa);
+    }
+    
+    public Wycieczkowiec(PortMorski lokalizacja){
+        super(lokalizacja.getPolozenie(), (int) (3 + Math.random() * 10), lokalizacja, null);
+        this.miejsca = 2 + (int) (Math.random()*18);
+        this.pasazerowie = new LinkedList<>();
+        System.out.println("WSZYSTKO OK");
+        
+        for (int i=0; i<this.miejsca/2; i++){
+            Podrozny p = new Podrozny(lokalizacja);
+//            this.pasazerowie.add(p);
+            ((PortMorski)this.getNajblizszyCel()).getOdwiedzajacy().add(p);
+            Thread t = new Thread(p);
+            t.setDaemon(true);
+            t.start();
+        }
     }
 
 
