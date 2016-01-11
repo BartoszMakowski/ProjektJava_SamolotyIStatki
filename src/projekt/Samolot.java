26a7e43b721a4fcff791b97bf57eec76f85d61bb
Bartosz <baboshpl@gmail.com>
2016-01-11 02:07:28 +0100

    przeniesiono losowanie trasy do metody klasy nadrzędnej

--- src/projekt/Samolot.java
+++ src/projekt/Samolot.java
@@ -7,26 +7,18 @@ import java.util.List;
  * Created by bartosz on 18.10.15.
  */
 public abstract class Samolot extends Pojazd {
-    private Paliwo paliwo;
+//    private Paliwo paliwo;
     private int liczebnoscPersonelu;
     private boolean usterka;
-    private boolean czekanie;
-
-
+    
     public Samolot(Polozenie polozenie, int predkosc, Lotnisko najblizszyCel, List<Lokalizacja> trasa) {
         super(polozenie, predkosc, najblizszyCel, trasa);
         this.usterka = false;
-        int i = (int) (Math.random() * Swiat.trasy.get("" + polozenie.getX() + "_" + polozenie.getY()).size());
-        List<Lokalizacja> mojaTrasa = new LinkedList<>();
-        mojaTrasa.addAll(Swiat.trasy.get("" + polozenie.getX() + "_" + polozenie.getY()).get(i));
-        this.setTrasa(mojaTrasa);
+        losujTrase(polozenie);
     }
-
-    public void tankuj(){}
 
     public void zglosUsterke(){
         this.usterka = true;
     }
-
-    public void lec(){}
+    
 }
