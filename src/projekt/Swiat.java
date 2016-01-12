7a6c0243dd7f23052c64f95a43ea86dcbef08b89
Bartosz <baboshpl@gmail.com>
2016-01-12 21:28:32 +0100

    zmieniono modyfikatory dostępu HashMap z publicznych na prywatne, dodano dla nich gettery

--- src/projekt/Swiat.java
+++ src/projekt/Swiat.java
@@ -6,6 +6,7 @@
 package projekt;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import javafx.application.Application;
@@ -23,11 +24,12 @@ public class Swiat extends Application {
     private static HashMap<String, Lotnisko> lotniskaCywilne;
     private static HashMap<String, Lotnisko> lotniskaWojskowe;
     private static HashMap<String, PortMorski> portyMorskie;
+    private static ArrayList<Lokalizacja> miasta;
     private static HashMap<String, Podrozny> pasazerowie;
-    public static HashMap<String, Lokalizacja> lokalizacje;
-    public static HashMap<String, Skrzyzowanie> skrzyzowania;
-    public static HashMap<String, ArrayList<LinkedList<Lokalizacja>>> trasy;
-    public static HashMap<String, LinkedList<Lokalizacja>> rozklady;
+    private static HashMap<String, Lokalizacja> lokalizacje;
+    private static HashMap<String, Skrzyzowanie> skrzyzowania;
+    private static HashMap<String, ArrayList<LinkedList<Lokalizacja>>> trasy;
+    private static HashMap<String, LinkedList<Lokalizacja>> rozklady;
     private static HashMap<String, Samolot> samoloty;
     private static HashMap<String, Statek> statki;
 //    public static SamolotPasazerski sp;
@@ -67,6 +69,34 @@ public class Swiat extends Application {
      */
     public static HashMap<String, Statek> getStatki() {
         return statki;
+    }
+
+    /**
+     * @return the lokalizacje
+     */
+    public static HashMap<String, Lokalizacja> getLokalizacje() {
+        return lokalizacje;
+    }
+
+    /**
+     * @return the lotniskaWojskowe
+     */
+    public static HashMap<String, Lotnisko> getLotniskaWojskowe() {
+        return lotniskaWojskowe;
+    }
+
+    /**
+     * @return the skrzyzowania
+     */
+    public static HashMap<String, Skrzyzowanie> getSkrzyzowania() {
+        return skrzyzowania;
+    }
+
+    /**
+     * @return the trasy
+     */
+    public static HashMap<String, ArrayList<LinkedList<Lokalizacja>>> getTrasy() {
+        return trasy;
     }
 
     
@@ -116,10 +146,10 @@ public class Swiat extends Application {
                 Lotnisko murzynno = new Lotnisko(460, 400, "Murzynno", 2, TypPortu.WOJSKOWY);
                 Lotnisko pasy = new Lotnisko(250, 460, "Pasy Krótkie", 2, TypPortu.WOJSKOWY);
                 
-                lotniskaWojskowe.put("30_320", alcatraz);
-                lotniskaWojskowe.put("250_20", powidz);
-                lotniskaWojskowe.put("460_400", murzynno);
-                lotniskaWojskowe.put("250_460", pasy);
+                getLotniskaWojskowe().put("30_320", alcatraz);
+                getLotniskaWojskowe().put("250_20", powidz);
+                getLotniskaWojskowe().put("460_400", murzynno);
+                getLotniskaWojskowe().put("250_460", pasy);
                 
 //                TWORZENIE PORTÓW MORSKICH
                 
@@ -138,40 +168,40 @@ public class Swiat extends Application {
 //                TWORZENIE SKRZYŻOWAŃ MORSKICH
                 
                 Skrzyzowanie sm1 = new Skrzyzowanie(275, 145, "SM1");
-                skrzyzowania.put("275_145", sm1);
+                getSkrzyzowania().put("275_145", sm1);
                 
 //                TWORZENIE SKRZYŻOWAŃ POWIETRZNYCH
                 
                 Skrzyzowanie s1 = new Skrzyzowanie(365, 20, "S1");
-                skrzyzowania.put("365_20", s1);
+                getSkrzyzowania().put("365_20", s1);
                 Skrzyzowanie s3 = new Skrzyzowanie(250, 200, "S3");
-                skrzyzowania.put("250_200", s3);
+                getSkrzyzowania().put("250_200", s3);
                 Skrzyzowanie s2 = new Skrzyzowanie(30, 200, "S2");
-                skrzyzowania.put("30_200", s2);
+                getSkrzyzowania().put("30_200", s2);
                 Skrzyzowanie s4 = new Skrzyzowanie(550, 200, "S4");
-                skrzyzowania.put("550_200", s4);
+                getSkrzyzowania().put("550_200", s4);
                 Skrzyzowanie s5 = new Skrzyzowanie(90, 320, "S5");
-                skrzyzowania.put("90_320", s5);
+                getSkrzyzowania().put("90_320", s5);
                 
                 Skrzyzowanie s6 = new Skrzyzowanie(90, 400, "S6");
-                skrzyzowania.put("90_400", s6);
+                getSkrzyzowania().put("90_400", s6);
                 Skrzyzowanie s7 = new Skrzyzowanie(250, 400, "S7");
-                skrzyzowania.put("250_400", s7);
+                getSkrzyzowania().put("250_400", s7);
                 Skrzyzowanie s8 = new Skrzyzowanie(365, 400, "S8");
-                skrzyzowania.put("365_400", s8);
+                getSkrzyzowania().put("365_400", s8);
                 Skrzyzowanie s9 = new Skrzyzowanie(90, 460, "S9");
-                skrzyzowania.put("90_460", s9);
+                getSkrzyzowania().put("90_460", s9);
                 Skrzyzowanie s10 = new Skrzyzowanie(365, 460, "S10");
-                skrzyzowania.put("365_460", s10);
+                getSkrzyzowania().put("365_460", s10);
                 
                 Skrzyzowanie s11 = new Skrzyzowanie(460, 460, "S11");
-                skrzyzowania.put("460_460", s11);
+                getSkrzyzowania().put("460_460", s11);
                 Skrzyzowanie s12 = new Skrzyzowanie(550, 460, "S12");
-                skrzyzowania.put("550_460", s12);
+                getSkrzyzowania().put("550_460", s12);
                 Skrzyzowanie s13 = new Skrzyzowanie(250, 550, "S13");
-                skrzyzowania.put("250_550", s13);
+                getSkrzyzowania().put("250_550", s13);
                 Skrzyzowanie s14 = new Skrzyzowanie(460, 550, "S14");
-                skrzyzowania.put("460_550", s14);
+                getSkrzyzowania().put("460_550", s14);
                 
                 brzysko.dodajDrogowskaz(new Drogowskaz(brzysko, s1));
                 brzysko.dodajDrogowskaz(new Drogowskaz(brzysko, s4));
@@ -277,10 +307,12 @@ public class Swiat extends Application {
 //                s1.dodajDrogowskaz(new Drogowskaz(40, niebieskie, Kierunek.LEWO));
 //                czerwone.dodajDrogowskaz(new Drogowskaz(70, s1, Kierunek.GORA));
                 
-                lokalizacje.putAll(lotniskaCywilne);
-                lokalizacje.putAll(lotniskaWojskowe);
-                lokalizacje.putAll(portyMorskie);
-                lokalizacje.putAll(skrzyzowania);
+                getLokalizacje().putAll(lotniskaCywilne);
+                getLokalizacje().putAll(getLotniskaWojskowe());
+                getLokalizacje().putAll(portyMorskie);
+                getLokalizacje().putAll(getSkrzyzowania());
+//                miasta.addAll( lotniskaCywilne);
+//                miasta.addAll((Collection<? extends Lokalizacja>) portyMorskie);
 
                 System.out.println("Utworzone lokalizacje: ");
                 for (Lokalizacja l : getLotniska().values()){
@@ -392,12 +424,12 @@ public class Swiat extends Application {
                 
                 
                 
-                trasy.put("550_20", tr550_20);
-                trasy.put("365_200", tr365_200);
-                trasy.put("365_550", tr365_550);
-                trasy.put("550_550", tr550_550);
-                trasy.put("550_360", tr550_360);
-                trasy.put("90_550", tr90_550);
+                getTrasy().put("550_20", tr550_20);
+                getTrasy().put("365_200", tr365_200);
+                getTrasy().put("365_550", tr365_550);
+                getTrasy().put("550_550", tr550_550);
+                getTrasy().put("550_360", tr550_360);
+                getTrasy().put("90_550", tr90_550);
                 
                 
                 trasa = new LinkedList<>();
@@ -406,7 +438,7 @@ public class Swiat extends Application {
                 trasa.add(borowno);
                 tr275_20.add(trasa);
                 
-                trasy.put("275_20", tr275_20);
+                getTrasy().put("275_20", tr275_20);
                 
                 
                 
@@ -420,7 +452,7 @@ public class Swiat extends Application {
                 trasa.add(alcatraz);
                 tr250_20.add(trasa);
                 
-                trasy.put("250_20", tr250_20);       
+                getTrasy().put("250_20", tr250_20);       
                 
             }
         };
