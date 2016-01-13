d39afd3d652d734a6c514bfded158d0b90585ff2
Bartosz <baboshpl@gmail.com>
2016-01-13 01:35:37 +0100

    testowo wprowadzono losowanie trasy algorytmem Dijkstry

--- src/projekt/SamolotPasazerski.java
+++ src/projekt/SamolotPasazerski.java
@@ -1,7 +1,11 @@
 package projekt;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import javafx.scene.image.ImageView;
 
 /**
@@ -93,6 +97,78 @@ public class SamolotPasazerski extends Samolot implements Pasazerski{
         this.pasazerowie = new LinkedList<>();
         super.usun();
         
+    }
+    
+    public void znajdzTrase(Lokalizacja skad){
+        LinkedList<Lokalizacja> znalezionaTrasa = new LinkedList<>();
+        HashMap<String, Lokalizacja> punkty = new HashMap<>();
+        HashMap<String, Lokalizacja> poprzednik = new HashMap<>();
+        HashMap<String, Integer> odleglosc = new HashMap<>();
+        Lokalizacja l;
+        
+        ArrayList<Lotnisko> listaLotnisk = new ArrayList<>();
+        listaLotnisk.addAll(Swiat.getLotniskaCywilne().values());
+               
+        punkty.putAll(Swiat.getLokalizacje());
+//        for (String s : Swiat.getLotniskaWojskowe().keySet()){
+//            punkty.remove(s);
+//        }
+        
+        for (String s : punkty.keySet()){
+            odleglosc.put(s, 99999);
+        }
+//        System.out.println(odleglosc);
+        
+        PriorityQueue<String> kolejka = new PriorityQueue<String>(punkty.keySet().size(), new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if ( odleglosc.get(o1) < odleglosc.get(o2) ){
+                    return -1;
+                }
+                else if ( odleglosc.get(o1) > odleglosc.get(o2) ){
+                    return 1;
+                }
+                return 0;                           
+            }
+        });
+               
+        
+                
+        odleglosc.replace(skad.getPolozenie().getX() + "_" + skad.getPolozenie().getY(), 0);
+        
+        kolejka.addAll(punkty.keySet());
+        while (kolejka.peek() != null){
+            l = punkty.get(kolejka.poll());
+            int odlegloscTutaj = odleglosc.get(l.getPolozenie().getX() + "_" + l.getPolozenie().getY());
+//            System.out.println(odlegloscTutaj);
+            
+//            System.out.println(l.getOdleglosci().size());
+            for (Drogowskaz d : l.getOdleglosci()){
+                if(odleglosc.get(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY()) >
+                         odlegloscTutaj + d.getOdleglosc()){
+//                    System.out.println("ZAMIANA!");
+                    odleglosc.replace(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY(),
+                            odlegloscTutaj + d.getOdleglosc());
+                    kolejka.add(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY());
+                    poprzednik.put(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY(), l);
+//                    System.out.println(poprzednik.get(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY()).getNazwa());
+                }
+            }
+            
+        }
+//        System.out.println(poprzednik.get("90_550").getNazwa());
+        
+        l = listaLotnisk.get((int)(listaLotnisk.size()*Math.random()));
+
+        while(l != skad){
+            znalezionaTrasa.addFirst(l);
+            System.out.println(l);
+            l = poprzednik.get(l.getPolozenie().getX() + "_" + l.getPolozenie().getY());
+        }
+        znalezionaTrasa.addFirst(l);
+        
+        getTrasa().addAll(znalezionaTrasa);
+                     
     }
    
        
