285da0363086d4e1ea7f4a2243e46e19dc8a47aa
Bartosz <baboshpl@gmail.com>
2016-01-14 00:55:03 +0100

    zaimplementowano losowanie trasy oraz metodę run()

--- src/projekt/SamolotWojskowy.java
+++ src/projekt/SamolotWojskowy.java
@@ -1,7 +1,11 @@
 package projekt;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import javafx.scene.image.ImageView;
 
 /**
@@ -35,7 +39,103 @@ public class SamolotWojskowy extends Samolot {
         return uzbrojenie;
     }
     
+    @Override
+    public void znajdzTrase(Lokalizacja skad){
+        LinkedList<Lokalizacja> znalezionaTrasa = new LinkedList<>();
+        HashMap<String, Lokalizacja> punkty;
+        HashMap<String, Lokalizacja> poprzednik = new HashMap<>();
+        HashMap<String, Integer> odleglosc = new HashMap<>();
+        Lokalizacja l;
+        
+        ArrayList<Lotnisko> listaLotnisk = new ArrayList<>();
+        listaLotnisk.addAll(Swiat.getLotniskaWojskowe().values());
+               
+        punkty = Swiat.getLokalizacje();
+//        for (String s : Swiat.getLotniskaWojskowe().keySet()){
+//            punkty.remove(s);
+//        }
+        
+        for (String s : punkty.keySet()){
+            odleglosc.put(s, 99999);
+        }
+//        System.out.println(odleglosc);
+        
+        PriorityQueue<String> kolejka = new PriorityQueue<String>(punkty.keySet().size(), new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if ( odleglosc.get(o1) < odleglosc.get(o2) ){
+                    return -1;
+                }
+                else if ( odleglosc.get(o1) > odleglosc.get(o2) ){
+                    return 1;
+                }
+                return 0;                           
+            }
+        });
+               
+        
+                
+        odleglosc.replace(skad.getPolozenie().getX() + "_" + skad.getPolozenie().getY(), 0);
+        
+        kolejka.addAll(punkty.keySet());
+        while (kolejka.peek() != null){
+            l = punkty.get(kolejka.poll());
+            int odlegloscTutaj = odleglosc.get(l.getPolozenie().getX() + "_" + l.getPolozenie().getY());
+//            System.out.println(odlegloscTutaj);
+            
+//            System.out.println(l.getOdleglosci().size());
+            for (Drogowskaz d : l.getOdleglosci()){
+                if(odleglosc.get(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY()) >
+                         odlegloscTutaj + d.getOdleglosc()){
+//                    System.out.println("ZAMIANA!");
+                    odleglosc.replace(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY(),
+                            odlegloscTutaj + d.getOdleglosc());
+                    kolejka.add(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY());
+                    poprzednik.put(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY(), l);
+//                    System.out.println(poprzednik.get(d.getDokad().getPolozenie().getX() + "_" + d.getDokad().getPolozenie().getY()).getNazwa());
+                }
+            }
+            
+        }
+//        System.out.println(poprzednik.get("90_550").getNazwa());
+        
+        l = listaLotnisk.get((int)(listaLotnisk.size()*Math.random()));
+        while(l.equals(skad)){
+            l = listaLotnisk.get((int)(listaLotnisk.size()*Math.random()));
+        }    
+        
+        while(l != skad){
+            znalezionaTrasa.addFirst(l);
+            System.out.println(l);
+            l = poprzednik.get(l.getPolozenie().getX() + "_" + l.getPolozenie().getY());
+        }
+        znalezionaTrasa.addFirst(l);
+        
+        getTrasa().clear();
+        getTrasa().addAll(znalezionaTrasa);
+                     
+    }
+    
+    @Override
+    public void run(){
+//        Image gpojazd;
+            while(isDzialaj())
+            {
+                ruszaniePojazdu();
+                przemieszczaniePojazdu();
+                if (getNajblizszyCel() instanceof Lotnisko){
+                    if (((Lotnisko)getNajblizszyCel()).getRodzaj().equals(TypPortu.WOJSKOWY)){
+                        this.getNajblizszyCel().stopujPojazd(this);
+                    }
+                }
+                zwolnijPole();
+                konczenieTrasyPojazdu();
+                obslugaNaMiejscu();                                                       
+            }        
+    }
+}
+    
 //    public javafx.scene.image.ImageView getObrazek() {
 //        return getObrazek();
 //    }
-}
+
